20305c4b3093a60c06d6d535664694bb22675789
Louis Godfrey <lginn26@users.noreply.github.com>
2019-04-10 09:14:45 -0400

    Added core file

---
+++ platforming.py
@@ -0,0 +1,51 @@
+# Imports
+import pygame
+import math
+import random
+
+# Initialize game engine
+pygame.init()
+
+# Window
+SIZE = (800, 600)
+TITLE = "Platforming Experiment"
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption(TITLE)
+
+# Timer
+clock = pygame.time.Clock()
+refresh_rate = 60
+
+# Colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ORANGE = (255, 125 , 0)
+
+# Game loop
+done = False
+
+while not done:
+    # Event processing (React to key presses, mouse clicks, etc.)
+    ''' for now, we'll just check to see if the X is clicked '''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # Game logic (Check for collisions, update points, etc.)
+
+    # Drawing code (Describe the picture. It isn't actually drawn yet.)
+    
+
+    # Update screen (Actually draw the picture in the window.)
+    pygame.display.flip()
+
+
+    # Limit refresh rate of game loop 
+    clock.tick(refresh_rate)
+
+
+# Close window and quit
+pygame.quit()
